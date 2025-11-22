da29bfe8c1c5b7271c10da77ee592d9f4d4e5045
Asadcoder786 <anonymousustad798@gmail.com>
2025-11-22 09:43:18 +0500

    4 question left only i will complete it later

---
+++ As-11/app.js
@@ -0,0 +1,313 @@
+console.log("js is running");
+// let newDate = new Date(2026,11,24,15,40,0)
+// console.log(newDate)
+// const now = new Date();
+// console.log(now);
+// console.log(newDate.getFullYear());
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDate());
+// // console.log(newDate.getDate);
+// console.log(newDate.getHours());
+// console.log(newDate.getMinutes());
+// console.log(newDate.getSeconds());
+// console.log(newDate.getMilliseconds());
+
+
+// chapter 35 to 38
+// Q1
+
+// function myDate () {
+//     let myDate = new Date()
+//     console.log(myDate)
+// }
+
+// myDate()
+
+// Q2
+
+// function fullName(){
+// let firstName = prompt("Enter Your First Name")
+// let lastName = prompt("Enter YOur Last Name")
+// let fullName = firstName + " " + lastName
+// console.log("First Name:",firstName);
+// console.log("Last Name:", lastName );
+// console.log("Full Name:",fullName)
+// alert(fullName)
+// }
+// fullName()
+
+// Q3
+
+
+
+// function add(){
+//     let num1 = +prompt("Enter first number",5)
+//     let num2 = +prompt("Enter second number",5)
+
+//     let sum = num1 + num2
+//    return console.log("Return Value",sum);
+// }
+
+// add()
+
+// Q4
+
+
+// function threeValue(a,b,operator) {
+//   if ( operator == "+") {
+//     return a + b;
+//   }else if (operator == "-"){
+//     return a-b ; 
+//   }else if (operator == "*") {
+//     return a * b ; 
+//   }else if (operator == "/") {
+//     return a/b;
+//   }
+    
+// }
+// console.log(threeValue(10,13,"/"))
+
+
+// Q5  
+
+// function numSquare (a) {
+//     return a * a
+// }
+
+// let square = numSquare(8)
+// console.log(square)
+
+// Q6
+
+
+
+// function factorial (a) {
+//    return a**2
+// }
+
+// console.log("Result:",factorial(6));
+
+
+// Q7
+
+// function middle () {
+//     let a = +prompt("Enter the First Value")
+// let b = +prompt("Enter the last Value")
+// for ( let i = a ; i<= b ; i++) {
+//     console.log(i)
+// }
+// }
+// middle()
+
+// Q8
+
+// function hyp(base,per) {
+//     let hyp = base + per ;
+//     console.log("Hypoteneus:" ,hyp);
+//     function sq (){
+//         return hyp ** 2
+        
+//     }
+//     console.log(sq())
+// }
+// hyp(5,10)
+
+// Q9
+
+
+// function value () {
+//     let w = 6;
+//     let h = 10;
+//     let result = w * h;
+//     return console.log("Result:",result)
+// }
+// value()
+
+
+// Q10
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
